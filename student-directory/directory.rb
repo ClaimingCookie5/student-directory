1b3ba014723c98c30e792b10a3fb42406d2d2f62
EC2 Default User <ec2-user@ip-172-31-17-28.us-east-2.compute.internal>
2021-07-20 11:00:57 +0000

    added methods to directory.rb

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,18 +1,30 @@
 students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates"
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
 ]
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each { |student| puts student }
-puts "Overall, we have #{students.count} great students"
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each { |name| puts name }
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
