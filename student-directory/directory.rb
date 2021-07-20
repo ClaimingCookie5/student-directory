8fafc1f864ecad495a1243f4a5ce712b8b3df909
EC2 Default User <ec2-user@ip-172-31-17-28.us-east-2.compute.internal>
2021-07-20 12:25:39 +0000

    step 8 - task 2

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,34 +1,38 @@
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {:name => name, :cohort => :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
-end
-
+students = [
+  {:name => "Dr. Hannibal Lecter", :cohort => :november},
+  {:name => "Darth Vader", :cohort => :november},
+  {:name => "Nurse Ratched", :cohort => :november},
+  {:name => "Michael Corleone", :cohort => :november},
+  {:name => "Alex DeLarge", :cohort => :november},
+  {:name => "The Wicked Witch of the West", :cohort => :november},
+  {:name => "Terminator", :cohort => :november},
+  {:name => "Freddy Krueger", :cohort => :november},
+  {:name => "The Joker", :cohort => :november},
+  {:name => "Joffrey Baratheon", :cohort => :november},
+  {:name => "Norman Bates", :cohort => :november}
+]
 def print_header
-  puts "The students of Villains Academy"
+  puts "Search for a student"
   puts "-------------"
 end
 
 def print(students)
-  students.each_with_index { |student, index| puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)" }
+  students_begin_with = []
+  letter = gets.chomp
+  students.each do |student|
+    if student[:name].start_with?(letter.upcase)
+      students_begin_with << student[:name]
+    end
+  end
+  puts students_begin_with
+  puts "There are #{students_begin_with.count} students that begin with the letter '#{letter.upcase}'"
 end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
+
 print_header
 print(students)
 print_footer(students)
-
-
-  
