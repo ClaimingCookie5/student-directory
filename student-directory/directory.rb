4f5e2869ec7fdd28be7490d7c93697a37364a9f8
EC2 Default User <ec2-user@ip-172-31-17-28.us-east-2.compute.internal>
2021-07-23 11:29:55 +0000

    added load student info method

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,4 +1,37 @@
 @students = []
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
+    puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+    case selection
+      when "1"
+        input_students
+      when "2"
+        show_students
+      when "3"
+        save_students_info
+      when "4"
+        load_students_info
+      when "9"
+        exit
+      else
+        puts "I don't know what you mean, try again"
+    end
+end
+
 def print_header
   center_length = 48
   puts "The students of Villains Academy".center(center_length)
@@ -8,13 +41,11 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
-    puts "Please enter the chort of #{name}"
-    month = gets.chomp
-    @students << {:name => name.capitalize, :cohort => month.capitalize}
+    @students << {:name => name.capitalize, :cohort => :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   @students
 end
@@ -38,38 +69,6 @@ def print_footer
   end 
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list to students.csv"
-    puts "4. Load the list from students.csv"
-    puts "9. Exit"
-end
-
-def process(selection)
-    case selection
-      when "1"
-        input_students
-      when "2"
-        show_students
-      when "3"
-        save_students_info
-      when "4"
-        load_students_info
-      when "9"
-        exit
-      else
-        puts "I don't know what you mean, try again"
-    end
-end
-
 def show_students
     print_header
     print_students_list
@@ -86,7 +85,7 @@ def save_students_info
   file.close
 end
 
-def load_students_info
+def load_students_info(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
@@ -94,5 +93,19 @@ def load_students_info
   end
   file.close
 end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students_info(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
   
