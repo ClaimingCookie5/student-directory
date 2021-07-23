be679a2681fb1a5503a821a6b8312f6b1425e2ef
EC2 Default User <ec2-user@ip-172-31-17-28.us-east-2.compute.internal>
2021-07-23 10:49:12 +0000

    added load student info method

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -49,6 +49,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
 end
 
@@ -60,6 +61,8 @@ def process(selection)
         show_students
       when "3"
         save_students_info
+      when "4"
+        load_students_info
       when "9"
         exit
       else
@@ -79,6 +82,15 @@ def save_students_info
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students_info
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {:name => name, :cohort => cohort.to_sym}
   end
   file.close
 end
